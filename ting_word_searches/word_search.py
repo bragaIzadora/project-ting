66536961916c90741e3127be784c5e0ed43551c1
Izadora Braga <izadora_abraga@outlook.com>
2024-06-17 16:26:30 -0300

    requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -25,4 +25,23 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        occurrences = []
+
+        for line_number, line in enumerate(file_data["linhas_do_arquivo"], 1):
+            if word.lower() in line.lower():
+                occurrences.append({
+                    "linha": line_number,
+                    "conteudo": line.strip()
+                })
+
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": occurrences
+            })
+
+    return result
