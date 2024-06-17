594e9eb59f4b305f8ca204ecec7dac046f0238af
Izadora Braga <izadora_abraga@outlook.com>
2024-06-17 16:23:22 -0300

    requisito 7 em teste

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,27 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        occurrences = []
+
+        for line_number, line in enumerate(file_data["linhas_do_arquivo"], 1):
+            line_words = line.lower().strip().split()
+            if any(
+                word.lower() == line_word.strip(",.!?;:")
+                for line_word in line_words
+            ):
+                occurrences.append({"linha": line_number})
+
+        if occurrences:
+            result.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_data["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return result
 
 
 def search_by_word(word, instance):
