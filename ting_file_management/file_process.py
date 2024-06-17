3c6795eb2313d2d17de796b5aaaab114f66334ff
Izadora Braga <izadora_abraga@outlook.com>
2024-06-17 14:59:16 -0300

    requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,25 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in instance._items:
+        if item['nome_do_arquivo'] == path_file:
+            return
+
+    lines = txt_importer(path_file)
+    if lines is None:
+        return
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+
+    instance.enqueue(file_info)
+
+    print(file_info)
+
 
 
 def remove(instance):
